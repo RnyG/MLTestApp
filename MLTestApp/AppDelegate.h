37bc9856be104726d1f352288400c9b793d17d95
Rhonny Gonzalez <rhonny.g@live.com>
2018-06-09 21:40:00 -0400

    Initial Commit

---
+++ MLTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MLTestApp
+//
+//  Created by Rhonny Gonzalez on 9/6/18.
+//  Copyright © 2018 Rhonny Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MLTestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MLTestApp
+//
+//  Created by Rhonny Gonzalez on 9/6/18.
+//  Copyright © 2018 Rhonny Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
